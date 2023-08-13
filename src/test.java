466d0fefae9dafee8a20df0d05e6c08a17fd8df1
zafer cansiz <zfrcnsz1@gmail.com>
2023-08-13 23:37:04 +0400

    githuba 2. gonderilen kodlar

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("1.gun kodlari yazildi");
+        System.out.println("2.gun kodlari yazildi");
 
     }
 
