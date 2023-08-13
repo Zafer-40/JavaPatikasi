d105b745605a9aae219279caef76c20ddf400bed
zafer cansiz <zfrcnsz1@gmail.com>
2023-08-14 00:39:15 +0400

    herseyi toparladik milad burasi

--- src/test.java
+++ src/test.java
@@ -2,7 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("1.gun kodlari yazildi");
-        System.out.println("1.gune donduk 2. gun kodlari yazildi ");
+        System.out.println("2.gun kodlari guncellendi");
+        System.out.println("3.gun kodlari yazdildi");
 
     }
 
